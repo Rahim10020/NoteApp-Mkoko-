004ba7b693d345feb9ccabdc0302b63a192cbbeb
Rahim10020 <rahialighi@gmail.com>
2025-11-25 12:52:26 +0000

    amelioration de la palette de couleur du dark

--- lib/theme/dark_mode.dart
+++ lib/theme/dark_mode.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+// Palette de couleurs sombre plus moderne
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
-    surface: const Color(0xFF1F1F1F),
-    primary: const Color(0xFF404040),
-    secondary: const Color(0xFF595959),
-    inversePrimary: Colors.white,
+    surface: const Color(0xFF000000),
+    primary: const Color(0xFF1C1C1E),
+    secondary: const Color(0xFF8E8E93),
+    inversePrimary: const Color(0xFFFFFFFF),
   ),
 );
