a2db7e37f76907fa2ff1b5e536a4f7f3a5ec1beb
Rahim10020 <rahialighi@gmail.com>
2025-11-25 14:44:24 +0000

    utilisation d'une couleur principale

--- lib/theme/light_mode.dart
+++ lib/theme/light_mode.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     surface: const Color(0xFFF5F5F7),
-    primary: const Color(0xFFFFFFFF),
-    secondary: const Color(0xFF8E8E93),
+    primary: const Color(0xFF3C82F6),
+    secondary: const Color(0xFFF5F5F7),
     inversePrimary: const Color(0xFF1C1C1E),
   ),
 );
